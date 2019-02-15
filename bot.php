f6afdada2c35e71480586e6a4e97734df59a47ae
Nattapong Suwannatrai <brender99@gmail.com>
2019-02-15 23:27:48 +0700

    Test1

--- bot.php
+++ bot.php
@@ -54,7 +54,7 @@ if (!is_null($events['events'])) {
             // Build message to reply back
             $messages = [
                 'type' => 'text',
-                'text' => $events
+                'text' => 'Non Text'
             ];
 
             // Make a POST Request to Messaging API to reply to sender
