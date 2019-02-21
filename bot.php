1a60243fd872bd0d5c6de96aebf5b4bbecfa5738
Nattapong Suwannatrai <brender99@gmail.com>
2019-02-21 23:58:06 +0700

    Test1

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
             }
             else
             {
-                $reply = 'What is' . $text;
+                $reply = 'What is "' . $text . '"?';
             }
 
 			// Build message to reply back
