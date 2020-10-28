45c67f7601b5f797050a75f60a757434e1246496
brender99 <brender99@gmail.com>
2020-10-28 18:39:07 +0700

    Test1

--- bot_bak.php
+++ bot_bak.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'Wwm2DAh0s7CLhm/o15DNx51IYHHa4sShW/k46CHfdm3Mc1eenakOvVIw/1z+mDfzaaG55en3d+h+fOEplDOoqHEhJ26rSiQTSYA9+/wrpAZ2OZmdLbeT2o4EfK84a2NhtrgPgEeU5udw9AUItPITwdB04t89/1O/w1cDnyilFU=';
+$access_token = '/Wwm2DAh0s7CLhm/o15DNx51IYHHa4sShW/k46CHfdm3Mc1eenakOvVIw/1z+mDfzaaG55en3d+h+fOEplDOoqHEhJ26rSiQTSYA9+/wrpAZ2OZmdLbeT2o4EfK84a2NhtrgPgEeU5udw9AUItPITwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content XXX
 $content = file_get_contents('php://input');
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 
 			if($text == 'Hi' || $text == 'hi' || $text == 'Hello' || $text == 'hello')
             {
-                $reply = 'สวัสดีครับ1';
+                $reply = 'สวัสดีครับ';
             }
             else
             {
